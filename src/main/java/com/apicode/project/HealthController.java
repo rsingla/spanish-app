fa2c8f09f8a2b783b77eae3fdec457f85def3448
Rajeev Singla <rajeev1.singla@gmail.com>
2019-08-13 02:51:59 -0700

    Added health check

---
+++ src/main/java/com/apicode/project/HealthController.java
@@ -0,0 +1,29 @@
+package com.apicode.project;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+import java.util.*;
+
+@RestController
+public class HealthController {
+ 
+   
+    @GetMapping("/")
+    public @ResponseBody String getHealth() {
+        return "Health is passed";
+    }
+}
